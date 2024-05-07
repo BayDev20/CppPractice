9104e84d6177c269eb896adf7aad5989a1d8eee4
Cody <152105436+BayDev20@users.noreply.github.com>
2024-05-07 13:46:28 -0700

    Update mod7.cpp

--- mod7.cpp
+++ mod7.cpp
@@ -3,7 +3,7 @@
 // Author      : Cody Beggs
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Thread work with mutexes
 //============================================================================
 
 #include <iostream>
